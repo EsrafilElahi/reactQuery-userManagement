26fc0f8061a6232f560f25997bf8f1bfbf65d28c
esrafilelahi <essinho1121@gmail.com>
2023-07-20 21:36:24 +0330

    add index style

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
-import App from './App.tsx';
+import App from './App.jsx';
+import "./styles/index.css";
+
 
 const fetchDefaultUsers = async () => {
   // fetch users
