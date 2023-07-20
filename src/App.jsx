bef5a8eb4b0cb7f1cb589449711a20a2a8b6c6af
esrafilelahi <essinho1121@gmail.com>
2023-07-20 21:30:19 +0330

    add routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
+// eslint-disable-next-line prettier/prettier
+import WithLayout from "components/layout/WithLayout";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Users from "./pages/Users";
+import Home from "./pages/Home";
 
 
-function App() {
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home title="Home" />,
+  },
+  {
+    path: "/users",
+    element: <Users title="Users" />,
+  },
+  // {
+  //   path: "users/:id",
+  //   element: <SerieDetailsLayout title="User Detail" />,
+  // },
+]);
+
+const App = () => {
   return (
     <>
-      <div>hi react</div>
+      <RouterProvider router={router} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import App from './App.tsx';
+
+const fetchDefaultUsers = async () => {
+  // fetch users
+}
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      queryKey: ["users"],
+      queryFn: fetchDefaultUsers,
+      initialData: [],
+      retry: 3,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
